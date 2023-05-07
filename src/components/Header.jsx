bd7410d616a3f4d8ba93455c0f4a9276b1ca2ce7
Nitin Singh <Nitinwebdev5@gmail.com>
2023-05-07 16:44:08 +0530

    fixed some bugs

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 export default class Header extends Component {
-    constructor(props){
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
+
+  greetPeopls = () => {
+    alert("Welcome to 30 Days Of React Challenge, 2023");
+  };
+
   render() {
-    // destructure the props 
-    const {welcome, title,subtitle,author,date} = this.props.data;
+    const { welcome, title, subtitle, author, date } = this.props.data;
     return (
-        <header>
-      
+      <header>
+        <div>
+          <h1>The classComponents</h1>
           <h1>{welcome}</h1>
           <h2>{title}</h2>
           <h3>{subtitle}</h3>
-          <p>{`${author.firstName} ${author.lastName}`}</p>
-          <small>{date}</small>
-        
-        </header>
-    )
+          <p>
+            {author.firstName} {author.lastName}
+          </p>
+          <p>
+            <small> {date} </small>
+          </p>
+          <button onClick={this.greetPeopls}>Greet</button>
+        </div>
+      </header>
+    );
   }
 }
